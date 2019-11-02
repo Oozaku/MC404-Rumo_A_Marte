fd0bd34008edf4343a9fa973092c85e51515d72a
SerodioJ <48109057+SerodioJ@users.noreply.github.com>
2019-11-01 21:19:11 -0300

    Update loco.c

--- loco.c
+++ loco.c
@@ -1,4 +1,4 @@
 /*Willian Takayuki Ozako - RA 207267
   João Alberto Moreira Seródio - RA 218548*/
   
-  /*Implementação das rotinas de controle presentes em api_robot.h*/
+  /*Utiliza as rotinas descritas em api_robot.h para enviar os comandos para o robô*/
