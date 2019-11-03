1a9efbb41f167f7913e37fd77f7b49db9596017d
João Seródio <serodio@DESKTOP-NNMNHA1.localdomain>
2019-11-03 15:17:07 -0200

    Syscalls 16, 17, 18,19 e 20

--- soul.s
+++ soul.s
@@ -8,6 +8,121 @@
 int_handler:
     # Tratamento de interrupcoes
 
+
+    int_handler_Return:  #A partir daqui restaurar o contexto e retornar ao código
+
+#Syscalls
+syscall16:
+    li t0, 0xFFFF0020 
+    sw zero, 0(t0)   #Inicia leitura do sensor de ultrassom
+    sys16_While:     #Verificando se o valor em 0xFFFF0020 foi atualizado
+        lw t1, 0(t0)
+        beqz t1, sys16_While
+    lw a0, 4(t0)     #0xFFFF0024
+    j int_handler_Return
+
+syscall17:
+    #Verifica qual o servo a ter o ângulo alterado
+    beqz a0, sys17_Base
+    li t0, 1
+    beq a0, t0, sys17_Mid
+    li t0, 2
+    beq a0, t0, sys17_Mid
+    li a0, -1   #Caso servo ID seja inválido
+    j int_handler_Return
+    #Servo ID 0 - Base (16-116)
+    sys17_Base:
+    #Validação do ângulo
+    li t0, 116
+    bgt a1, t0, sys17_InvalidAngle
+    li t0, 16
+    blt a1, t0, sys17_InvalidAngle
+    li t0, 0xFFFF001E  #Ângulo servo-motor 1 (base)
+    sb a1, 0(t0)
+    li a0, 0
+    j int_handler_Return
+    #Servo ID 1 - Mid (52-90)
+    sys17_Mid:
+    #Validação do ângulo
+    li t0, 90
+    bgt a1, t0, sys17_InvalidAngle
+    li t0, 52
+    blt a1, t0, sys17_InvalidAngle
+    li t0, 0xFFFF001D  #Ângulo servo-motor 2 (mid)
+    sb a1, 0(t0)
+    li a0, 0
+    j int_handler_Return
+    #Servo ID 2 - Top (0-156)
+    sys17_top:
+    #Validação do ângulo
+    li t0, 156
+    bgt a1, t0, sys17_InvalidAngle
+    bltz a1, sys17_InvalidAngle
+    li t0, 0xFFFF001C  #Ângulo servo-motor 3 (top)
+    sb a1, 0(t0)
+    li a0, 0
+    j int_handler_Return
+    #ângulo Inválido
+    sys17_InvalidAngle:
+    li a0, -2
+    j int_handler_Return
+    
+
+syscall18:
+    beqz a0, sys18_Eng1
+    li t0, 1
+    beq a0, to, sys18_Eng2
+    li a0, -1    #Caso o ID do motor seja inválido
+    j int_handler_Return
+    #Configura torque do motor 1
+    sys18_Eng1:
+    li t0, 0xFFFF001A  #Torque motor 1
+    sh a1, 0(t0)
+    li a0, 0
+    j int_handler_Return
+    #Configura torque do motor 2
+    sys18_Eng2:
+    li t0, 0xFFFF0018  #Torque motor 2
+    sh a1, 0(t0)
+    li a0, 0
+    j int_handler_Return
+
+syscall19:
+    li t0, 0xFFFF0004
+    sw zero, 0(t0)  #Inicia a leitura da posição do robô
+    sys19_While:    #Verificando se o valor em 0xFFFF0020 foi atualizado
+        lw t1, 0(t0)
+        beqz t1, sys19_While
+    lw t1, 4(t0)  # 0xFFFF0008 -> x
+    sw t1, 0(a0)  #Armazena x
+    lw t1, 8(t0)  # 0xFFFF000C -> y
+    sw t1, 4(a0)  #Armazena y
+    lw t1, 12(t0) # 0xFFFF0010 -> z
+    sw t1, 8(a0)  #Armazena z
+    j int_handler_Return
+
+syscall20:
+    li t0, 0xFFFF0004
+    sw zero, 0(t0)  #Inicia a leitura da posição do robô
+    sys20_While:    #Verificando se o valor em 0xFFFF0020 foi atualizado
+        lw t1, 0(t0)
+        beqz t1, sys20_While
+    lw t1, 16(t0)  # 0xFFFF0014
+    andi t0, t1, 1023  #z
+    sw t0, 8(a0)       #Armazena z
+    srli t1, t1, 10
+    andi t0, t1, 1023  #y
+    sw t0, 4(a0)       #Armazena y
+    srli t1, t1, 10    #x
+    sw t1, 0(a0)       #Armazena x
+    j int_handler_Return
+
+syscall21:
+
+syscall22:
+
+syscall64:
+
 _start:
     # Falta configurar o GPT
     # Iniciando motores com valor 0
