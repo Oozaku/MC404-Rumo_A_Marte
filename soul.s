7b54d76509f2aaccbcb231870621ec6cacefc4da
Oozaku <48293220+Oozaku@users.noreply.github.com>
2019-11-04 17:28:06 -0300

    Add files via upload
    syscall21, syscall22, salva e recupera contexto, GPT

--- soul.s
+++ soul.s
@@ -7,12 +7,110 @@
 
 int_handler:
     # Tratamento de interrupcoes
+    # Salvando contexto de t0~6 e a1~7 e end. pilha em s0
+    csrrw s0, mscratch, s0
+    sw a1, 0(s0)
+    sw a2, 4(s0)
+    sw a3, 8(s0)
+    sw a4, 12(s0)
+    sw a5, 16(s0)
+    sw a6, 20(s0)
+    sw a7, 24(s0)
+    sw t0, 28(s0)
+    sw t1, 32(s0)
+    sw t2, 36(s0)
+    sw t3, 40(s0)
+    sw t4, 44(s0)
+    sw t5, 48(s0)
+    sw t6, 52(s0)
+    # Descobrindo a causa da interrupcao ou da excecao
+    csrr t0, mcause
+    bgt zero, t0, Tratar_excecao
+    j tratar_interrupcao
+
+        Tratar_excecao:     # Chamada de syscall para valores 8 e 9 e 
+                            # para n >= 12 ou n <= 7 necessita parar o programa
+            andi t0, t0, 0x7FFFFFFF
+            li t1, 8                    # Syscall pelo modo usuario
+            beq t0, t1, chamadaSyscall
+            li t1, 9                    # Syscall pelo modo Super
+            beq t0, t1, chamadaSyscall
+            li t1, 7
+            ble t0, t1, sairPrograma 
+            li t1, 12
+            bge t0, t1, sairPrograma 
+
+        chamadaSyscall:     # Identificando codigo da syscall
+                li t0, 16
+                li t1, 17
+                li t2, 18
+                li t3, 19
+                li t4, 20
+                li t5, 21
+                li t6, 22
+                beq a7, t0, syscall16
+                beq a7, t0, syscall17
+                beq a7, t0, syscall18
+                beq a7, t0, syscall19
+                beq a7, t0, syscall20
+                beq a7, t0, syscall21
+                beq a7, t0, syscall22
+                li t0, 64
+                beq a7, t0, syscall64
+                j sairPrograma          # Erro: codigo de syscall desconhecida
+    
+        tratar_interrupcao:
+            andi t0, t0, 0x7FFFFFFF
+            li t1, 3
+            beq t0, t1, tratarGPT
+            j SoRecuperaContexto
+
+        tratarGPT:
+            # Configurando para o GPT interromper no proximo 1ms 
+            li t0, 1
+            li t1, 0xFFFF0100	
+            sw t0, 0(t1)
+            # Registrar a interrupcao de GPT como ja tratada
+            li t1, 0xFFFF0104
+            sw zero, 0(t1)
+            # Adicionando 1ms no tempoReal
+            la t0, tempoReal
+            lw t1, 0(t0)
+            addi t1, t1, 1
+            sw t1, 0(t0)
+            j SoRecuperaContexto
+        
+        sairPrograma:   # Implementar mais tarde o codigo de saida 
+            j SoRecuperaContexto
 
 
-    int_handler_Return:  #A partir daqui restaurar o contexto e retornar ao código
+    int_handler_Return:  #A partir daqui restaurar o contexto e retornar ao codigo
+        # Apontar para a instrucao depois daquela que chamou a syscall
+        csrr t0, mepc
+        addi t0, t0, 4
+        csrs mepc, t0
+
+        SoRecuperaContexto:
+            # Restaurando contexto
+            lw a1, 0(s0)
+            lw a2, 4(s0)
+            lw a3, 8(s0)
+            lw a4, 12(s0)
+            lw a5, 16(s0)
+            lw a6, 20(s0)
+            lw a7, 24(s0)
+            lw t0, 28(s0)
+            lw t1, 32(s0)
+            lw t2, 26(s0)
+            lw t3, 40(s0)
+            lw t4, 44(s0)
+            lw t5, 48(s0)
+            lw t6, 52(s0)
+            csrrw s0, mscratch, s0
+            mret
 
 #Syscalls
-syscall16:
+syscall16: # read_ultrasonic_sensor
     li t0, 0xFFFF0020 
     sw zero, 0(t0)   #Inicia leitura do sensor de ultrassom
     sys16_While:     #Verificando se o valor em 0xFFFF0020 foi atualizado
@@ -21,7 +119,7 @@ syscall16:
     lw a0, 4(t0)     #0xFFFF0024
     j int_handler_Return
 
-syscall17:
+syscall17:  # set_servo_angles
     #Verifica qual o servo a ter o ângulo alterado
     beqz a0, sys17_Base
     li t0, 1
@@ -68,7 +166,7 @@ syscall17:
     j int_handler_Return
     
 
-syscall18:
+syscall18:  # set_engine_torque
     beqz a0, sys18_Eng1
     li t0, 1
     beq a0, to, sys18_Eng2
@@ -87,7 +185,7 @@ syscall18:
     li a0, 0
     j int_handler_Return
 
-syscall19:
+syscall19: # read_gps
     li t0, 0xFFFF0004
     sw zero, 0(t0)  #Inicia a leitura da posição do robô
     sys19_While:    #Verificando se o valor em 0xFFFF0020 foi atualizado
@@ -101,7 +199,7 @@ syscall19:
     sw t1, 8(a0)  #Armazena z
     j int_handler_Return
 
-syscall20:
+syscall20: # read_gyroscope
     li t0, 0xFFFF0004
     sw zero, 0(t0)  #Inicia a leitura da posição do robô
     sys20_While:    #Verificando se o valor em 0xFFFF0020 foi atualizado
@@ -117,14 +215,24 @@ syscall20:
     sw t1, 0(a0)       #Armazena x
     j int_handler_Return
 
-syscall21:
+syscall21:  # get_time
+    la t0, tempoReal
+    lw a0, 0(t0)
+    j int_handler_Return
 
-syscall22:
+syscall22:  # set_time
+    la t0, tempoReal
+    sw a0, 0(t0)
+    j int_handler_Return
 
-syscall64:
+syscall64: # write
+
 
 _start:
-    # Falta configurar o GPT
+    # Config. GPT para interromper a cada 1ms
+    li t0, 0xFFFF0100	
+    li t1, 1
+    sw t1, 0(t0)
     # Iniciando motores com valor 0
     li t0, 0xFFFF0018 # motor 1
     li t1, 0xFFFF001A # motor 2
@@ -170,3 +278,5 @@ _start:
 
 reg_buffer:
     .skip 132
+tempoReal:
+    .skip 4
